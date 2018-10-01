90d698cfbda44b64368c34dcbf910e70855bab26
dremee <kirilllezhnin@mail.ru>
2018-10-01 12:19:13 +0500

    complete simple js logic

--- Question-Game-master/script/script.js
+++ Question-Game-master/script/script.js
@@ -1,9 +1,18 @@
-const questionField = document.getElementById("question");
-const start = document.getElementById("start-game");
+const questionField = document.getElementById("question"),
+      start = document.getElementById("start-game"),
+      answers = document.querySelector("#answers_button"),
+      btnTrue = document.querySelector("#true"),
+      btnFalse = document.querySelector("#false"),
+      url = "https://opentdb.com/api.php?amount=1&type=boolean",
+      btnAnswers = document.getElementsByClassName('btn_answer');
+
+
 let questionRes = {};
 
+
+// Get question from server
 function getQuestion(){
-    axios.get("https://opentdb.com/api.php?amount=1&type=boolean")
+    axios.get(url)
     .then((req)=>{
         return req.data
     })
@@ -14,12 +23,24 @@ function getQuestion(){
         return results[0]
     })
     .then((conditions)=>{
-        // questionField.textContent = question;
-        questionRes = conditions;
+        // paste question to question field
         questionField.innerHTML = conditions.question;
+        
+        // show buttons with answer
+        answers.classList.remove('hide');
+        answers.classList.add('show');
+        
+        // add answers in questionRes variable 
+        questionRes.correct = conditions.correct_answer;
+        questionRes.incorrect = conditions.incorrect_answers[0];
+
+        // console.log(conditions.correct_answer);
+        // console.log(conditions.incorrect_answers[0]);
+
+
     })
     .catch((err)=>{
-        console.log(err)
+        questionField.innerHTML = "Something gone wrong, try again!";
     })
 }
 
@@ -27,11 +48,48 @@ function getQuestion(){
 
 
 
-
+// use getQuestion wile press btn start
 start.onclick = function(){
     getQuestion();
+    start.textContent = "Next question";
+    resetClasses();    
 }
 
-questionField.onclick = function(){
-    console.log(questionField.textContent)
+// create logic of game
+// we need to take userAnswer, and check, if user answer === answer
+
+btnTrue.onclick = () => {
+    checkCorrect();
+    btnTrue.classList.add('userChoice');
+}
+
+btnFalse.onclick = () => {
+    checkCorrect();
+    btnFalse.classList.add('userChoice');
+}
+
+// Logic functions
+
+// check answer and get correct style classes to buttons
+checkCorrect = () => {
+    if(questionRes.correct === "True"){
+        btnTrue.classList.add('correct');
+        btnFalse.classList.add('incorrect');
+    } else {
+        btnTrue.classList.add('incorrect');
+        btnFalse.classList.add('correct');
+    }
+}
+
+
+// reset classes
+
+resetClasses = () => {
+    btnTrue.classList.remove('correct');
+    btnTrue.classList.remove('incorrect');
+    btnTrue.classList.remove('userChoice');
+
+    btnFalse.classList.remove('correct');
+    btnFalse.classList.remove('incorrect');
+    btnFalse.classList.remove('userChoice');
 }
